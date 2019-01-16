2dd6153f4678944cfe35863745e677eedfa0d55e
Ivan Miatselski2 <Ivan_Miatselski2@epam.com>
2019-01-16 04:59:32 +0300

    Implement web-server

--- news-app/routes/news.js
+++ news-app/routes/news.js
@@ -1,45 +1,147 @@
-var express = require('express');
-var fs = require('fs');
-var router = express.Router();
+const express = require('express');
+const bodyParser = require("body-parser");
+const createError = require('http-errors');
+const { logger } = require('../logger/winston-logger.js');
+const fs = require('fs');
+const util = require('util');
 
-const jsonFilePath = '../store/news.json';
+const router = express.Router();
+const jsonParser = bodyParser.json();
+
+const jsonFilePath = './store/news.json';
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
 
 router.use(express.json());
 
 router.get('/', (request, response, next) => {
-    const news = readJSONFile();
-    response.send(news);
+    logger.info(`GET request to ${request.hostname}${request.baseUrl}`);
+
+    readFileAsync(jsonFilePath, 'utf-8')
+        .then((content) => {
+            const obj = JSON.parse(content);
+            response.send(obj);
+        })
+        .catch((err) => {
+            logger.error(err);
+            next(createError(500));
+        });
 });
 
-router.get('/:id', function(request, response, next) {
-    response.send(newsJson);
+router.get('/:id', (request, response, next) => {
+    logger.info(`GET request to ${request.hostname}${request.baseUrl}`);
+
+    readFileAsync(jsonFilePath, 'utf-8')
+        .then((content) => {
+            const news = JSON.parse(content);
+            for(var i = 0; i < news.length; i++) {
+                if (news[i].id == request.params.id) {
+                    response.send(news[i]);
+                }
+            }
+
+            next(createError(404));
+        })
+        .catch((err) => {
+            logger.error(err);
+            next(createError(500));
+        });
 });
 
-router.post('/:id', (request, response, next) => {
+router.post('/', jsonParser, (request, response, next) => {
+    logger.info(`POST request to ${request.hostname}${request.baseUrl}`);
 
+    if (!request.body){
+        logger.error(`request body not defined`);
+        next(createError(404));
+    }
+
+    var newContent = { name: request.body.name, url: request.body.url };
+
+    readFileAsync(jsonFilePath, 'utf-8')
+        .then((content) => {
+            var news = JSON.parse(content);
+            var maxId = Math.max.apply(Math, news.map( x => x.id));
+            newContent.id = maxId ? maxId + 1 : 1;
+            
+            news.push(newContent);
+            writeFileAsync(jsonFilePath, JSON.stringify(news))
+                .then(() => response.send(`Added new content with name ${newContent.name}`))
+                .catch((err) => { 
+                    logger.error(err);
+                    next(createError(500))
+                });
+        })
+        .catch((err) => {
+            logger.error(err);
+            next(createError(500));
+        });
 });
 
-router.put('/:id', (request, response, next) => {
+router.put('/:id', jsonParser, (request, response, next) => {
+    logger.info(`PUT request to ${request.hostname}${request.baseUrl}`);
 
+    if (!request.body){
+        next(createError(404));
+    }
+
+    readFileAsync(jsonFilePath, 'utf-8')
+        .then((content) => {
+            let news = JSON.parse(content);
+
+            let entityToUpdate;
+            for(let i = 0; i < news.length; i++) {
+                if (news[i].id == request.params.id){
+                    entityToUpdate = news[i];
+                    break;
+                }
+            }
+
+            if (entityToUpdate) {
+                entityToUpdate.name = request.body.name;
+                entityToUpdate.url = request.body.url;
+
+                writeFileAsync(jsonFilePath, JSON.stringify(news))
+                    .then(() => response.send(`Updated entity with id: '${request.params.id}'`))
+                    .catch(() => next(createError(500)));
+            }
+            else {
+                next(createError(404));
+            }
+        })
+        .catch(() => {
+            logger.error(err);
+            next(createError(500));
+        });
 });
 
 router.delete('/:id', (request, response, next) => {
+    logger.info(`DELETE request to ${request.hostname}${request.baseUrl}`);
 
+    readFileAsync(jsonFilePath, 'utf-8')
+        .then((content) => {
+            var news = JSON.parse(content);
+            felteredNews = news.filter(x => x.id != request.params.id);
+
+            if (felteredNews.length == news.length){
+                response.send(`News with id: '${request.params.id}' not found`)
+            }
+            else {
+                writeFileAsync(jsonFilePath, JSON.stringify(felteredNews))
+                    .then(() => {
+                        response.send(`News with id: '${request.params.id}' was deleted`);
+                    })
+                    .catch((err) => { 
+                        logger.error(err);
+                        next(createError(500)); 
+                    });
+            }
+        })
+        .catch((err) => {
+            logger.error(err);
+            next(createError(500))
+        });
 });
 
-function readJSONFile() {
-    let news = [];
-
-    fs.readFile(jsonFilePath, function (err, data) {
-        if (err) {
-            throw new Error(`Error of processing json file with destination ${jsonFilePath}`)
-        }
-    
-        var obj = JSON.parse(data);
-        news.push(obj);
-    });
-
-    return news;
-}
 
 module.exports = router;
