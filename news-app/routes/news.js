40a48a8d0915874e4942bdc3a3e605966149a32f
Ivan Miatselski2 <Ivan_Miatselski2@epam.com>
2019-01-23 02:05:49 +0300

    Unify error handler

--- news-app/routes/news.js
+++ news-app/routes/news.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require("body-parser");
 const createError = require('http-errors');
+const wrap = require('async-middleware').wrap;
 const { logger } = require('../logger/winston-logger.js');
 const { JsonFileRepository } = require('../business_logic/services/jsonFileRepository.js');
 const { check, validationResult } = require('express-validator/check');
@@ -19,38 +20,26 @@ const validateBody = () => {
 
 router.use(express.json());
 
-router.get('/', async (request, response, next) => {
+router.get('/', wrap(async (request, response, next) => {
     logger.info(`GET request to ${request.hostname}${request.baseUrl}`);
 
-    try {
-        const data = await jsonRepository.readItem();       
-        response.send(data);
-    }
-    catch (err) {
-        logger.error(err);
-        return next(createError(500));
-    }
-});
+    const data = await jsonRepository.readItem();       
+    response.send(data);
+}));
 
-router.get('/:id', async (request, response, next) => {
+router.get('/:id', wrap(async (request, response, next) => {
     logger.info(`GET request to ${request.hostname}${request.baseUrl}`);
 
-    try {
-        const item = await jsonRepository.readItem(request.params.id);
-        
-        if (item != null){
-            response.send(item);
-        }
-        
-        return next(createError(404));
+    const item = await jsonRepository.readItem(request.params.id);
+    
+    if (item != null){
+        response.send(item);
     }
-    catch (err) {
-        logger.error(err);
-        return next(createError(500));
-    }
-});
+    
+    return next(createError(404));
+}));
 
-router.post('/', validateBody(), async (request, response, next) => {
+router.post('/', validateBody(), wrap(async (request, response, next) => {
     logger.info(`POST request to ${request.hostname}${request.baseUrl}`);
     
     if (!validationResult(request).isEmpty()) {
@@ -59,17 +48,11 @@ router.post('/', validateBody(), async (request, response, next) => {
 
     let newContent = { name: request.body.name, url: request.body.url };
 
-    try {
-        await jsonRepository.createItem(newContent);
-        response.send(`Added new content with name ${newContent.name}`);
-    }
-    catch (err) {
-        logger.error(err);
-        return next(createError(500));
-    }
-});
+    await jsonRepository.createItem(newContent);
+    response.send(`Added new content with name ${newContent.name}`);
+}));
 
-router.put('/:id', validateBody(), async (request, response, next) => {
+router.put('/:id', validateBody(), wrap(async (request, response, next) => {
     logger.info(`PUT request to ${request.hostname}${request.baseUrl}`);
 
     if (!validationResult(request).isEmpty()) {
@@ -78,33 +61,21 @@ router.put('/:id', validateBody(), async (request, response, next) => {
 
     let newContent = { name: request.body.name, url: request.body.url };
 
-    try {
-        if (await jsonRepository.updateItem(request.params.id, newContent)) {
-            response.send(`Updated entity with id: '${request.params.id}'`);
-        }
-        
-        return next(createError(404));
+    if (await jsonRepository.updateItem(request.params.id, newContent)) {
+        response.send(`Updated entity with id: '${request.params.id}'`);
     }
-    catch (err) {
-        logger.error(err);
-        return next(createError(500));
-    }
-});
+    
+    return next(createError(404));
+}));
 
-router.delete('/:id', async (request, response, next) => {
+router.delete('/:id', wrap(async (request, response, next) => {
     logger.info(`DELETE request to ${request.hostname}${request.baseUrl}`);
 
-    try {
-        if (await jsonRepository.deleteItem(request.params.id)) {
-            response.send(`News with id: '${request.params.id}' was deleted`);
-        }
-        
-        return next(createError(404));
+    if (await jsonRepository.deleteItem(request.params.id)) {
+        response.send(`News with id: '${request.params.id}' was deleted`);
     }
-    catch (err) {
-        logger.error(err);
-        return next(createError(500));
-    }
-});
+    
+    return next(createError(404));
+}));
 
 module.exports = router;
